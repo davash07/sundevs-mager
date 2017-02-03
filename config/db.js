7a7cd6cf9766a7dc7e498a1184d7eb14d0b15b69
davash07 <davi9719@gmail.com>
2017-02-03 09:42:07 -0500

    Add

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@
  * Created by devios on 20/01/17.
  */
 module.exports = {
-    // 'url' : 'mongodb://Davash07:davash07@ds139959.mlab.com:39959/sundevs_manager/'
-    'url' : 'mongodb://127.0.0.1:27017/sundevs-manager/'
+    'url' : 'mongodb://Davash07:davash07@ds139959.mlab.com:39959/sundevs_manager/'
+    // 'url' : 'mongodb://127.0.0.1:27017/sundevs-manager/'
 };
